b2ccf24c9358491b01205ca281aa6ae56852d896
Mathias Hoarau <mathias@wuaro.com>
2022-09-09 15:58:02 +0200

    added controls

--- content/script.js
+++ content/script.js
@@ -1,7 +1,42 @@
 (() => {
+
+    let buttonsLoaded = false
+
+    const createButton = (label, seconds, first = false) => {
+
+        // Create button
+        const button = document.createElement('button')
+        button.innerHTML = label
+        button.className = "ytp-button ytp-custom-button"
+        if (first) button.style.marginLeft = "20px"
+
+        button.addEventListener('click', () => {
+            document.querySelector('video.video-stream').currentTime += seconds
+        })
+
+        return button
+    }
+
+    const createButtons = () => {
+
+        buttonsLoaded = true
+
+        const controlContainer = document.querySelector('.ytp-left-controls')
+
+        controlContainer.append(createButton("-15m", -60 * 15, true))
+        controlContainer.append(createButton("-1m", -60 ))
+        controlContainer.append(createButton("-10s", -10))
+        controlContainer.append(createButton("+10s", 10))
+        controlContainer.append(createButton("+1m", 60 ))
+        controlContainer.append(createButton("+15m", 60 * 15))
+    
+    }
     
     const updateAntiSpoil = (enabled) => {
         if (enabled) {
+
+            if (!buttonsLoaded) createButtons()
+
             document.body.classList.add('anti-spoil-enabled')
             document.body.classList.remove('anti-spoil-disabled')
         } else {
